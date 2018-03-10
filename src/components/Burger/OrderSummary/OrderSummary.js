545d66e4b963aeb95b532f49c01ee843569aa282
unknown <anthonyhseo@gmail.com>
2018-03-09 17:36:49 -0800

    refactored and optimized code

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -4,12 +4,6 @@ import Aux from '../../../hoc/Auxiliary/Auxiliary';
 import Button from '../../UI/Button/Button';
 
 class OrderSummary extends Component {
-  // This could be a functional component, does not have to be a class
-  componentWillUpdate() {
-    console.log('[OrderSummary] WillUpdate');
-  }
-
-
   render() {
     const ingredientSummary = Object.keys(this.props.ingredients)
       .map(igKey => {
